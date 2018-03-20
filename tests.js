8688c3d18452cf941ef7d0eb791bb7afa667c149
Justin Li <justinnhli@gmail.com>
2018-03-19 22:36:35 -0700

    add findClosestParent tests

--- tests.js
+++ tests.js
@@ -11,22 +11,162 @@ function assertEqual(actual, expected, msg) {
     );
 }
 
+function checkASTPosition(node, type, start_line, start_col, end_line, end_col) {
+    if (type === "Program") {
+        node = node.program;
+    } else if (type === "BlockStatement") {
+        node = node;
+    } else {
+        assert(false, "Unknown AST node type: " + node);
+    }
+    assertEqual(node.type, type, "Block type is wrong");
+    assertEqual(node.loc.start.line, start_line, "Start line is wrong");
+    assertEqual(node.loc.start.column, start_col, "Start line is wrong");
+    assertEqual(node.loc.end.line, end_line, "End line is wrong");
+    assertEqual(node.loc.end.column, end_col, "End column is wrong");
+}
+
 function testClosestParentAfterFunctionDefinition() {
     var ast = recast.parse([
         "function log(s) {",
         "    console.log(s);",
         "}",
     ].join("\n"));
-    var position = {
-        "line": 3,
-        "column": 1
-    }
+    var position = {"line": 3, "column": 1};
     var parent_node = brickEditor.findClosestParent(ast, position);
-    assertEqual(parent_node.program.type, "Program", "Block type is wrong");
-    assertEqual(parent_node.program.loc.start.line, 1, "Start line is wrong");
-    assertEqual(parent_node.program.loc.start.column, 0, "Start line is wrong");
-    assertEqual(parent_node.program.loc.end.line, 3, "End line is wrong");
-    assertEqual(parent_node.program.loc.end.column, 1, "End column is wrong");
+    checkASTPosition(parent_node, "Program", 1, 0, 3, 1);
+}
+
+function testClosestParentBeforeFunctionDefinition() {
+    var ast = recast.parse([
+        "function log(s) {",
+        "    console.log(s);",
+        "}",
+    ].join("\n"));
+    var position = {"line": 0, "column": 0};
+    var parent_node = brickEditor.findClosestParent(ast, position);
+    checkASTPosition(parent_node, "Program", 1, 0, 3, 1);
+}
+
+function testClosestParentBeforeFunctionOpenBrace() {
+    var ast = recast.parse([
+        "function log(s) {",
+        "    console.log(s);",
+        "}",
+    ].join("\n"));
+    var position = {"line": 0, "column": 0};
+    var parent_node = brickEditor.findClosestParent(ast, position);
+    checkASTPosition(parent_node, "Program", 1, 0, 3, 1);
+}
+
+function testClosestParentAfterFunctionOpenBrace() {
+    var ast = recast.parse([
+        "function log(s) {",
+        "    console.log(s);",
+        "}",
+    ].join("\n"));
+    var position = {"line": 0, "column": 0};
+    var parent_node = brickEditor.findClosestParent(ast, position);
+    checkASTPosition(parent_node, "BlockStatement", 1, 16, 3, 1);
+}
+
+function testClosestParentBeforeFunctionCloseBrace() {
+    var ast = recast.parse([
+        "function log(s) {",
+        "    console.log(s);",
+        "}",
+    ].join("\n"));
+    var position = {"line": 0, "column": 0};
+    var parent_node = brickEditor.findClosestParent(ast, position);
+    checkASTPosition(parent_node, "BlockStatement", 1, 16, 3, 1);
+}
+
+function testClosestParentBeforeFirstLine() {
+    var ast = recast.parse([
+        "function log(s) {",
+        "    console.log(1);",
+        "    console.log(2);",
+        "    console.log(3);",
+        "}",
+    ].join("\n"));
+    var position = {"line": 2, "column": 4};
+    var parent_node = brickEditor.findClosestParent(ast, position);
+    checkASTPosition(parent_node, "BlockStatement", 1, 16, 3, 1);
+}
+
+function testClosestParentAfterLastLine() {
+    var ast = recast.parse([
+        "function log(s) {",
+        "    console.log(1);",
+        "    console.log(2);",
+        "    console.log(3);",
+        "}",
+    ].join("\n"));
+    var position = {"line": 4, "column": 19};
+    var parent_node = brickEditor.findClosestParent(ast, position);
+    checkASTPosition(parent_node, "BlockStatement", 1, 16, 3, 1);
+}
+
+function testClosestParentBeforeMultipleLines() {
+    var ast = recast.parse([
+        "function log(s) {",
+        "    console.log(1);",
+        "    console.log(2);",
+        "    console.log(3);",
+        "}",
+    ].join("\n"));
+    var position = {"line": 3, "column": 4};
+    var parent_node = brickEditor.findClosestParent(ast, position);
+    checkASTPosition(parent_node, "BlockStatement", 1, 16, 3, 1);
+}
+
+function testClosestParentAfterMultipleLines() {
+    var ast = recast.parse([
+        "function log(s) {",
+        "    console.log(1);",
+        "    console.log(2);",
+        "    console.log(3);",
+        "}",
+    ].join("\n"));
+    var position = {"line": 3, "column": 19};
+    var parent_node = brickEditor.findClosestParent(ast, position);
+    checkASTPosition(parent_node, "BlockStatement", 1, 16, 3, 1);
+}
+
+function testClosestParentInVariable() {
+    var ast = recast.parse([
+        "function log(s) {",
+        "    console.log(1);",
+        "    console.log(2);",
+        "    console.log(3);",
+        "}",
+    ].join("\n"));
+    var position = {"line": 3, "column": 7};
+    var parent_node = brickEditor.findClosestParent(ast, position);
+    checkASTPosition(parent_node, "BlockStatement", 1, 16, 3, 1);
+}
+
+function testClosestParentInFunctionCall() {
+    var ast = recast.parse([
+        "function log(s) {",
+        "    console.log(1);",
+        "    console.log(2);",
+        "    console.log(3);",
+        "}",
+    ].join("\n"));
+    var position = {"line": 3, "column": 17};
+    var parent_node = brickEditor.findClosestParent(ast, position);
+    checkASTPosition(parent_node, "BlockStatement", 1, 16, 3, 1);
 }
 
 testClosestParentAfterFunctionDefinition();
+testClosestParentBeforeFunctionDefinition();
+testClosestParentBeforeFunctionOpenBrace();
+testClosestParentAfterFunctionOpenBrace();
+testClosestParentBeforeFunctionCloseBrace();
+testClosestParentBeforeFirstLine();
+testClosestParentAfterLastLine();
+testClosestParentBeforeMultipleLines();
+testClosestParentAfterMultipleLines();
+testClosestParentInVariable();
+testClosestParentInFunctionCall();

73354a43e23fb8329a3d2cd36fdd471def202285
Charlotte Cullip <charlotte.cullip@gmail.com>
2018-03-28 21:06:25 -0700

    add FindPreviousSibling test cases

--- tests.js
+++ tests.js
@@ -13,19 +13,16 @@ function assertEqual(actual, expected, msg) {
 }
 
 function checkASTPosition(node, type, start_line, start_col, end_line, end_col) {
-    /*if (type === "Program") {
-        node = node.program;
-    } else if (type === "BlockStatement") {
-        // node is already usable
+    // check if node is supposed to be null
+    if (node == null) {
+        assertEqual(node, type);
     } else {
-        assert(false, "Unknown AST node type: " + node);
-    }*/
-
-    assertEqual(node.type, type, "Block type is wrong");
-    assertEqual(node.loc.start.line, start_line, "Start line is wrong");
-    assertEqual(node.loc.start.column, start_col, "Start line is wrong");
-    assertEqual(node.loc.end.line, end_line, "End line is wrong");
-    assertEqual(node.loc.end.column, end_col, "End column is wrong");
+        assertEqual(node.type, type, "Block type is wrong");
+        assertEqual(node.loc.start.line, start_line, "Start line is wrong");
+        assertEqual(node.loc.start.column, start_col, "Start line is wrong");
+        assertEqual(node.loc.end.line, end_line, "End line is wrong");
+        assertEqual(node.loc.end.column, end_col, "End column is wrong");
+    } 
 }
 
 function testClosestParentNearBraces() {
@@ -35,32 +32,32 @@ function testClosestParentNearBraces() {
         "}",
     ].join("\n"));
     var position = null;
-    var parent_node = null;
+    var parentNode = null;
 
     // after function definition
     position = {"lineNumber": 3, "column": 1};
-    parent_node = brickEditor.findClosestParent(ast, position);
-    checkASTPosition(parent_node, "BlockStatement", 1, 16, 3, 1);
+    parentNode = brickEditor.findClosestParent(ast, position);
+    checkASTPosition(parentNode, "Program", 1, 0, 3, 1);
 
     // before function definition
     position = {"lineNumber": 1, "column": 0};
-    parent_node = brickEditor.findClosestParent(ast, position);
-    checkASTPosition(parent_node, "Program", 1, 0, 3, 1);
+    parentNode = brickEditor.findClosestParent(ast, position);
+    checkASTPosition(parentNode, "Program", 1, 0, 3, 1);
 
     // before function open brace
     position = {"lineNumber": 1, "column": 16};
-    parent_node = brickEditor.findClosestParent(ast, position);
-    checkASTPosition(parent_node, "BlockStatement", 1, 16, 3, 1);
+    parentNode = brickEditor.findClosestParent(ast, position);
+    checkASTPosition(parentNode, "BlockStatement", 1, 16, 3, 1);
 
     // after function open brace
     position = {"lineNumber": 1, "column": 17};
-    parent_node = brickEditor.findClosestParent(ast, position);
-    checkASTPosition(parent_node, "BlockStatement", 1, 16, 3, 1);
+    parentNode = brickEditor.findClosestParent(ast, position);
+    checkASTPosition(parentNode, "BlockStatement", 1, 16, 3, 1);
 
     // before function close brace
     position = {"lineNumber": 3, "column": 0};
-    parent_node = brickEditor.findClosestParent(ast, position);
-    checkASTPosition(parent_node, "BlockStatement", 1, 16, 3, 1);
+    parentNode = brickEditor.findClosestParent(ast, position);
+    checkASTPosition(parentNode, "BlockStatement", 1, 16, 3, 1);
 }
 
 function testClosestParentMultipleLines() {
@@ -72,37 +69,37 @@ function testClosestParentMultipleLines() {
         "}",
     ].join("\n"));
     var position = null;
-    var parent_node = null;
+    var parentNode = null;
 
     // before first line
     position = {"lineNumber": 2, "column": 4};
-    parent_node = brickEditor.findClosestParent(ast, position);
-    checkASTPosition(parent_node, "BlockStatement", 1, 16, 5, 1);
+    parentNode = brickEditor.findClosestParent(ast, position);
+    checkASTPosition(parentNode, "BlockStatement", 1, 16, 5, 1);
 
     // after last line
     position = {"lineNumber": 4, "column": 19};
-    parent_node = brickEditor.findClosestParent(ast, position);
-    checkASTPosition(parent_node, "BlockStatement", 1, 16, 5, 1);
+    parentNode = brickEditor.findClosestParent(ast, position);
+    checkASTPosition(parentNode, "BlockStatement", 1, 16, 5, 1);
 
     // before second line
     position = {"lineNumber": 3, "column": 4};
-    parent_node = brickEditor.findClosestParent(ast, position);
-    checkASTPosition(parent_node, "BlockStatement", 1, 16, 5, 1);
+    parentNode = brickEditor.findClosestParent(ast, position);
+    checkASTPosition(parentNode, "BlockStatement", 1, 16, 5, 1);
 
     // after second line
     position = {"lineNumber": 3, "column": 19};
-    parent_node = brickEditor.findClosestParent(ast, position);
-    checkASTPosition(parent_node, "BlockStatement", 1, 16, 5, 1);
+    parentNode = brickEditor.findClosestParent(ast, position);
+    checkASTPosition(parentNode, "BlockStatement", 1, 16, 5, 1);
 
     // in variable
     position = {"lineNumber": 3, "column": 7};
-    parent_node = brickEditor.findClosestParent(ast, position);
-    checkASTPosition(parent_node, "BlockStatement", 1, 16, 5, 1);
+    parentNode = brickEditor.findClosestParent(ast, position);
+    checkASTPosition(parentNode, "BlockStatement", 1, 16, 5, 1);
 
     // in function call
     position = {"lineNumber": 3, "column": 17};
-    parent_node = brickEditor.findClosestParent(ast, position);
-    checkASTPosition(parent_node, "BlockStatement", 1, 16, 5, 1);
+    parentNode = brickEditor.findClosestParent(ast, position);
+    checkASTPosition(parentNode, "BlockStatement", 1, 16, 5, 1);
 }
 
 function testClosestParentNested() {
@@ -131,44 +128,112 @@ function testClosestParentNested() {
         "}",
     ].join("\n"));
     var position = null;
-    var parent_node = null;
+    var parentNode = null;
 
     // in while keyword
     position = {"lineNumber": 5, "column": 7};
-    parent_node = brickEditor.findClosestParent(ast, position);
-    checkASTPosition(parent_node, "BlockStatement", 1, 16, 22, 1);
+    parentNode = brickEditor.findClosestParent(ast, position);
+    checkASTPosition(parentNode, "BlockStatement", 1, 16, 22, 1);
 
     // in while condition
     position = {"lineNumber": 5, "column": 12};
-    parent_node = brickEditor.findClosestParent(ast, position);
-    checkASTPosition(parent_node, "BlockStatement", 1, 16, 22, 1);
+    parentNode = brickEditor.findClosestParent(ast, position);
+    checkASTPosition(parentNode, "BlockStatement", 1, 16, 22, 1);
 
     // in while block
     position = {"lineNumber": 6, "column": 17};
-    parent_node = brickEditor.findClosestParent(ast, position);
-    checkASTPosition(parent_node, "BlockStatement", 5, 17, 18, 5);
+    parentNode = brickEditor.findClosestParent(ast, position);
+    checkASTPosition(parentNode, "BlockStatement", 5, 17, 18, 5);
 
     // in if keyword
     position = {"lineNumber": 9, "column": 9};
-    parent_node = brickEditor.findClosestParent(ast, position);
-    checkASTPosition(parent_node, "BlockStatement", 5, 17, 18, 5);
+    parentNode = brickEditor.findClosestParent(ast, position);
+    checkASTPosition(parentNode, "BlockStatement", 5, 17, 18, 5);
 
     // in if condition
     position = {"lineNumber": 9, "column": 16};
-    parent_node = brickEditor.findClosestParent(ast, position);
-    checkASTPosition(parent_node, "BlockStatement", 5, 17, 18, 5);
+    parentNode = brickEditor.findClosestParent(ast, position);
+    checkASTPosition(parentNode, "BlockStatement", 5, 17, 18, 5);
 
     // in if true block
     position = {"lineNumber": 12, "column": 25};
-    parent_node = brickEditor.findClosestParent(ast, position);
-    checkASTPosition(parent_node, "BlockStatement", 9, 24, 13, 9);
+    parentNode = brickEditor.findClosestParent(ast, position);
+    checkASTPosition(parentNode, "BlockStatement", 9, 24, 13, 9);
 
     // in if false block
     position = {"lineNumber": 14, "column": 3};
-    parent_node = brickEditor.findClosestParent(ast, position);
-    checkASTPosition(parent_node, "BlockStatement", 13, 15, 17, 9);
+    parentNode = brickEditor.findClosestParent(ast, position);
+    checkASTPosition(parentNode, "BlockStatement", 13, 15, 17, 9);
+
+    var ast = recast.parse([
+        'function Person(age) {',
+        '    if (age) {',
+        '        this.age = age;',
+        '    }',
+        '}'].join('\n'));
+
+    position = { "lineNumber": 3, "column": 23 };
+    parentNode = brickEditor.findClosestParent(ast, position);
+    checkASTPosition(parentNode, "BlockStatement", 2, 13, 4, 5);
 }
+
+function testFindPreviousSibling() {
+    var ast = recast.parse([
+        'function test(a) {',
+        '    var a = 3;',
+        '    if (a == 3) {',
+        '        print(5);',
+        '    } else {',
+        '            ',
+        '    }',
+        '    return a;',
+        '}'].join('\n'));
+    var position = null;
+    var prevSibling = null;
+
+    // after opening curly brace
+    position = { "lineNumber": 1, "column": 18 };
+    prevSibling = brickEditor.findPreviousSibling(ast, position);
+    checkASTPosition(prevSibling, null);
+
+    // before var a = 3
+    position = { "lineNumber": 2, "column": 4 };
+    prevSibling = brickEditor.findPreviousSibling(ast, position);
+    checkASTPosition(prevSibling, null);
+
+    // after var a = 3
+    position = { "lineNumber": 2, "column": 14 };
+    prevSibling = brickEditor.findPreviousSibling(ast, position);
+    checkASTPosition(prevSibling, "VariableDeclaration", 2, 4, 2, 14);
+
+    // before print(5)
+    position = { "lineNumber": 4, "column": 4 };
+    prevSibling = brickEditor.findPreviousSibling(ast, position);
+    checkASTPosition(prevSibling, null);
+
+    // after print(5)
+    position = { "lineNumber": 4, "column": 17 };
+    prevSibling = brickEditor.findPreviousSibling(ast, position);
+    checkASTPosition(prevSibling, "ExpressionStatement", 4, 8, 4, 17);
+
+    // in empty line in else statement
+    position = { "lineNumber": 6, "column": 12 };
+    prevSibling = brickEditor.findPreviousSibling(ast, position);
+    checkASTPosition(prevSibling, null);
+
+    // after closing curly brace of else statement
+    position = { "lineNumber": 7, "column": 5};
+    prevSibling = brickEditor.findPreviousSibling(ast, position);
+    checkASTPosition(prevSibling, "IfStatement", 3, 4, 7, 5);
+
+    // after closing curly brace of function
+    position = { "lineNumber": 9, "column": 1 };
+    prevSibling = brickEditor.findPreviousSibling(ast, position);
+    checkASTPosition(prevSibling, "FunctionDeclaration", 1, 0, 9, 1);
+}
+
 
 testClosestParentNearBraces();
 testClosestParentMultipleLines();
 testClosestParentNested();
+testFindPreviousSibling();

d2161a7d806c22671f2a4574dfe083492576dc21
Justin Li <justinnhli@gmail.com>
2018-04-15 16:19:25 -0700

    create global editor state

--- globals.js
+++ globals.js
@@ -3,9 +3,16 @@
 // the Monaco editor instance
 var editor = null; // eslint-disable-line no-unused-vars
 
-// whether the editor buffer is parsable
-var parsable = true; // eslint-disable-line no-unused-vars
-// the editor buffer at the last parsable point
-var parsableText = ""; // eslint-disable-line no-unused-vars
-// the position of the cursor/selection at the last parsable point
-var cursor = null; // eslint-disable-line no-unused-vars
+// eslint-disable-next-line no-unused-vars
+var editorState = {
+    // whether the editor buffer is currently parsable
+    parsable: null,
+    // the last valid parse of the buffer
+    parse: null,
+    // the editor buffer at the last parsable point
+    parsableText: "",
+    // whether the cursor was a selection at the last parsable point
+    hasSelected: null,
+    // the position of the cursor/selection at the last parsable point
+    cursor: null,
+};
